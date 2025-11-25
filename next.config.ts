0e1f37b16fe073a20cbbddfcda05edcdbcdd221e
chauthanhcuong2004 <chauthanhcuong2004@gmail.com>
2025-11-25 22:53:18 +0700

    #

--- next.config.ts
+++ next.config.ts
@@ -20,15 +20,15 @@ const nextConfig: NextConfig = {
             }
         ],
     },
-    // async redirects() {
-    //     return [
-    //         {
-    //             source: '/',
-    //             destination: '/home',
-    //             permanent: false
-    //         }
-    //     ];
-    // },
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/home',
+                permanent: false
+            }
+        ];
+    },
 };
 
 export default nextConfig;
